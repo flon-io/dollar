a84d3a65b30f6bb0255bfe1a6a99bcd1b40caf87
John Mettraux <jmettraux@gmail.com>
2014-11-16 06:57:15 +0900

    make fdol_dlup() available to dollar consumers

--- spec/dollar_spec.c
+++ spec/dollar_spec.c
@@ -12,13 +12,6 @@
 
 context "dollar"
 {
-  char *fd_lookup(const char *path, void *data)
-  {
-    //printf("      lookup: >%s<\n", path);
-    char *r = flu_list_get(data, path);
-    return r ? rdz_strdup(r) : NULL;
-  }
-
   before each
   {
     flu_dict *dict = flu_d(
@@ -43,7 +36,7 @@ context "dollar"
     it "returns a copy of the string, even if no expansion took place"
     {
       char *s = "quick brown fox";
-      char *s1 = fdol_expand(s, fd_lookup, dict);
+      char *s1 = fdol_expand(s, fdol_dlup, dict);
 
       expect(s != s1);
       expect(s === s1);
@@ -53,51 +46,51 @@ context "dollar"
 
     it "expands \"$(brown)\""
     {
-      expect(fdol_expand("$(brown)", fd_lookup, dict) ===f "fox");
+      expect(fdol_expand("$(brown)", fdol_dlup, dict) ===f "fox");
     }
     it "expands \".$(brown).\""
     {
-      expect(fdol_expand(".$(brown).", fd_lookup, dict) ===f ".fox.");
+      expect(fdol_expand(".$(brown).", fdol_dlup, dict) ===f ".fox.");
     }
     it "expands \"$(brown) $(lazy)\""
     {
-      expect(fdol_expand("$(brown) $(lazy)", fd_lookup, dict) ===f "fox dog");
+      expect(fdol_expand("$(brown) $(lazy)", fdol_dlup, dict) ===f "fox dog");
     }
     it "expands \"$($(l)$(z))\""
     {
-      expect(fdol_expand("$($(l)$(z))", fd_lookup, dict) ===f "dog");
+      expect(fdol_expand("$($(l)$(z))", fdol_dlup, dict) ===f "dog");
     }
 
     it "expands to a blank string if it doesn't find"
     {
-      expect(fdol_expand("<$(blue)>", fd_lookup, dict) ===f "<>");
+      expect(fdol_expand("<$(blue)>", fdol_dlup, dict) ===f "<>");
     }
 
     it "doesn't expand \"a)b\""
     {
-      expect(fdol_expand("a)b", fd_lookup, dict) ===f "a)b");
+      expect(fdol_expand("a)b", fdol_dlup, dict) ===f "a)b");
     }
     it "doesn't expand \"$xxx\""
     {
-      expect(fdol_expand("$xxx", fd_lookup, dict) ===f "$xxx");
+      expect(fdol_expand("$xxx", fdol_dlup, dict) ===f "$xxx");
     }
     it "doesn't expand \"x$xxx\""
     {
-      expect(fdol_expand("x$xxx", fd_lookup, dict) ===f "x$xxx");
+      expect(fdol_expand("x$xxx", fdol_dlup, dict) ===f "x$xxx");
     }
     it "doesn't expand \"$(nada||'$xxx)\""
     {
-      expect(fdol_expand("$(nada||'$xxx)", fd_lookup, dict) ===f "$xxx");
+      expect(fdol_expand("$(nada||'$xxx)", fdol_dlup, dict) ===f "$xxx");
     }
 
     it "accepts an escaped )"
     {
-      expect(fdol_expand("$(nada||'su\\)rf)", fd_lookup, dict) ===f ""
+      expect(fdol_expand("$(nada||'su\\)rf)", fdol_dlup, dict) ===f ""
         "su)rf");
     }
     it "accepts an escaped ) (deeper)"
     {
-      expect(fdol_expand("$(a||'$(nada||'su\\)rf))", fd_lookup, dict) ===f ""
+      expect(fdol_expand("$(a||'$(nada||'su\\)rf))", fdol_dlup, dict) ===f ""
         "su)rf");
     }
     it "accepts an escaped $"
@@ -106,47 +99,47 @@ context "dollar"
     {
       it "understands || (or)"
       {
-        expect(fdol_expand("$(blue||brown)", fd_lookup, dict) ===f "fox");
+        expect(fdol_expand("$(blue||brown)", fdol_dlup, dict) ===f "fox");
       }
       it "understands |r (reverse)"
       {
-        expect(fdol_expand("$(brown|r)", fd_lookup, dict) ===f "xof");
+        expect(fdol_expand("$(brown|r)", fdol_dlup, dict) ===f "xof");
       }
       it "understands |u (uppercase)"
       {
-        expect(fdol_expand("$(brown|u)", fd_lookup, dict) ===f "FOX");
+        expect(fdol_expand("$(brown|u)", fdol_dlup, dict) ===f "FOX");
       }
       it "understands |u|r"
       {
-        expect(fdol_expand("$(brown|u|r)", fd_lookup, dict) ===f "XOF");
+        expect(fdol_expand("$(brown|u|r)", fdol_dlup, dict) ===f "XOF");
       }
       it "understands |d (downcase)"
       {
-        expect(fdol_expand("$(black|d)", fd_lookup, dict) ===f "pug");
+        expect(fdol_expand("$(black|d)", fdol_dlup, dict) ===f "pug");
       }
 
       it "understands |1..-1"
       {
-        expect(fdol_expand("$(quick|1..-1)", fd_lookup, dict) ===f "ump");
+        expect(fdol_expand("$(quick|1..-1)", fdol_dlup, dict) ===f "ump");
       }
       it "understands |2"
       {
-        expect(fdol_expand("$(quick|2)", fd_lookup, dict) ===f "m");
+        expect(fdol_expand("$(quick|2)", fdol_dlup, dict) ===f "m");
       }
       it "understands |-3"
       {
-        expect(fdol_expand("$(quick|-3)", fd_lookup, dict) ===f "u");
+        expect(fdol_expand("$(quick|-3)", fdol_dlup, dict) ===f "u");
       }
 
       it "understands ||'text"
       {
-        expect(fdol_expand("$(nada||'text|u)", fd_lookup, dict) ===f "TEXT");
+        expect(fdol_expand("$(nada||'text|u)", fdol_dlup, dict) ===f "TEXT");
       }
 
       it "understands |c (capitalize)"
       {
-        expect(fdol_expand("$(ba|c)", fd_lookup, dict) ===f "Black Adder");
-        expect(fdol_expand("$(bs|c)", fd_lookup, dict) ===f "Black Sheep");
+        expect(fdol_expand("$(ba|c)", fdol_dlup, dict) ===f "Black Adder");
+        expect(fdol_expand("$(bs|c)", fdol_dlup, dict) ===f "Black Sheep");
       }
 
       it "understands |s/xx/yy/ (substitution filter)"
@@ -156,21 +149,21 @@ context "dollar"
     {
       it "understands |l>4 (length filter)"
       {
-        expect(fdol_expand("$(lazy|l>4||'none)", fd_lookup, dict) ===f "none");
-        expect(fdol_expand("$(lazy|l<4||'none)", fd_lookup, dict) ===f "dog");
+        expect(fdol_expand("$(lazy|l>4||'none)", fdol_dlup, dict) ===f "none");
+        expect(fdol_expand("$(lazy|l<4||'none)", fdol_dlup, dict) ===f "dog");
 
-        expect(fdol_expand("$(lazy|l<=3||'none)", fd_lookup, dict) ===f "dog");
-        expect(fdol_expand("$(lazy|l>=3||'none)", fd_lookup, dict) ===f "dog");
+        expect(fdol_expand("$(lazy|l<=3||'none)", fdol_dlup, dict) ===f "dog");
+        expect(fdol_expand("$(lazy|l>=3||'none)", fdol_dlup, dict) ===f "dog");
 
-        expect(fdol_expand("$(lazy|l=3||'none)", fd_lookup, dict) ===f "dog");
-        expect(fdol_expand("$(lazy|l=4||'none)", fd_lookup, dict) ===f "none");
-        expect(fdol_expand("$(lazy|l==3||'none)", fd_lookup, dict) ===f "dog");
-        expect(fdol_expand("$(lazy|l==4||'none)", fd_lookup, dict) ===f "none");
+        expect(fdol_expand("$(lazy|l=3||'none)", fdol_dlup, dict) ===f "dog");
+        expect(fdol_expand("$(lazy|l=4||'none)", fdol_dlup, dict) ===f "none");
+        expect(fdol_expand("$(lazy|l==3||'none)", fdol_dlup, dict) ===f "dog");
+        expect(fdol_expand("$(lazy|l==4||'none)", fdol_dlup, dict) ===f "none");
 
-        expect(fdol_expand("$(lazy|l!=4||'none)", fd_lookup, dict) ===f "dog");
-        expect(fdol_expand("$(lazy|l!=3||'none)", fd_lookup, dict) ===f "none");
-        expect(fdol_expand("$(lazy|l<>4||'none)", fd_lookup, dict) ===f "dog");
-        expect(fdol_expand("$(lazy|l<>3||'none)", fd_lookup, dict) ===f "none");
+        expect(fdol_expand("$(lazy|l!=4||'none)", fdol_dlup, dict) ===f "dog");
+        expect(fdol_expand("$(lazy|l!=3||'none)", fdol_dlup, dict) ===f "none");
+        expect(fdol_expand("$(lazy|l<>4||'none)", fdol_dlup, dict) ===f "dog");
+        expect(fdol_expand("$(lazy|l<>3||'none)", fdol_dlup, dict) ===f "none");
       }
 
       it "understands |m/xx/ (match filter)"
@@ -180,7 +173,7 @@ context "dollar"
     {
       it "understands |$(func)"
       {
-        expect(fdol_expand("$(lazy|$(func))", fd_lookup, dict) ===f "DOG");
+        expect(fdol_expand("$(lazy|$(func))", fdol_dlup, dict) ===f "DOG");
       }
     }
   }
